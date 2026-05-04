fe39e5e27f68c9fef94da5c5d88a539b0ef741c2
ImaniMusafiri88 <imanimusafiri08@gmail.com>
2026-05-04 12:39:57 +0200

    refactor: enforce widget.attribs'placeholder' in SignupForm fields

--- user_authentification_system/login/forms.py
+++ user_authentification_system/login/forms.py
@@ -12,3 +12,22 @@ class SignUpForm(UserCreationForm):
         ''' class Meta '''
         model = User
         fields = ['username', 'email', 'phone_number', 'password1', 'password2']
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+        self.fields['username'].widget.attrs.update({
+            'placeholder': 'Username'
+        })      
+        self.fields['phone_number'].widget.attrs.update({
+            'placeholder': '+257 XX XX XX XX'
+        })
+        self.fields['email'].widget.attrs.update({
+            'placeholder': 'account@xyz.com'
+        })
+        self.fields['password1'].widget.attrs.update({
+            'placeholder': 'Password'
+        })      
+        self.fields['password2'].widget.attrs.update({
+            'placeholder': 'Confirm your password'
+        })
