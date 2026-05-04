fbfd89fdd4dd641f14b047c24736f73144567445
ImaniMusafiri88 <imanimusafiri08@gmail.com>
2026-05-04 11:25:38 +0200

    refactor: remove required=True and blank=True constraint on phone_number field

--- user_authentification_system/login/models.py
+++ user_authentification_system/login/models.py
@@ -4,4 +4,4 @@ from django.contrib.auth.models import AbstractUser
 
 class CustomUser(AbstractUser):
     '''User create model'''
-    phone_number = models.CharField(max_length=20, blank=True, null=True)
+    phone_number = models.CharField(max_length=20)
